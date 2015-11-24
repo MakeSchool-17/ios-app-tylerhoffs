a41b646919b6dbc936d3c155ff17edcbec93c0a3
Unathi Chonco <choncou@gmail.com>
2015-11-23 16:55:10 -0800

    Initialise Xcode project

---
+++ Transit WiseTests/Transit_WiseTests.swift
@@ -0,0 +1,36 @@
+//
+//  Transit_WiseTests.swift
+//  Transit WiseTests
+//
+//  Created by Unathi Chonco on 11/23/15.
+//  Copyright © 2015 Transit Wise. All rights reserved.
+//
+
+import XCTest
+@testable import Transit_Wise
+
+class Transit_WiseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
