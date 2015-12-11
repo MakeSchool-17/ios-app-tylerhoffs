cc72573787a2cc3fdd36c9775b4b5897b7278fd8
tylerhoffs <tyler.hoffman@students.makeschool.com>
2015-12-11 10:36:43 -0800

    Add tweaks to home page

--- Transit Wise/ViewControllers/HomeViewController.swift
+++ Transit Wise/ViewControllers/HomeViewController.swift
@@ -13,7 +13,14 @@ import GoogleMaps
 class HomeViewController: UIViewController {
     
     
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var tripPlannerView: UIView!
     @IBOutlet weak var mainTableView: UITableView!
+    @IBOutlet weak var directionButton: UIButton!
+    var viewDown: Bool = false
+    @IBOutlet weak var tripPlannerBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var searchButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,14 +28,22 @@ class HomeViewController: UIViewController {
             longitude:28.2289275, zoom:12)
         let mapView = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
         
-        // Parallax Header
+        // Parallax Header Setup
         let header = mapView
-        
         mainTableView.parallaxHeader.view = header
         mainTableView.parallaxHeader.height = 400
         mainTableView.parallaxHeader.mode = MXParallaxHeaderMode.Fill
         mainTableView.parallaxHeader.minimumHeight = 200
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
 
+    }
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
     // MARK: - Table view data source
@@ -55,7 +70,103 @@ class HomeViewController: UIViewController {
         NSLog("progress %f", scrollView.parallaxHeader.progress)
     }
     
+    // MARK: - Button Actions
     
+    @IBAction func directionButtonTap(sender: UIButton) {
+        print("Button!")
+        if(!self.viewDown){
+            
+            
+            UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseOut, animations: {
+            
+            /*var tripPlannerFrame = self.tripPlannerView.frame
+            tripPlannerFrame.origin.y = 0
+            self.tripPlannerView.frame = tripPlannerFrame
+                
+            var tableViewFrame = self.mainTableView.frame
+            tableViewFrame.origin.y = tripPlannerFrame.height
+            self.mainTableView.frame = tableViewFrame */
+                var tripPlannerFrame = self.tripPlannerView.frame
+                tripPlannerFrame.origin.y = 0
+                
+                
+                
+                
+                self.tripPlannerView.frame = tripPlannerFrame
+
+            
+                
+  
+                }, completion: { finished in
+                    print("View Moved!")
+                    //self.viewDown = true
+                    self.tripPlannerBottomConstraint.constant = -156.00
+                    UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseOut, animations: {
+                        
+                        let tableHeader = self.mainTableView.parallaxHeader
+                        tableHeader.height = self.mainView.frame.height - 50
+                        self.mainTableView.parallaxHeader.height = tableHeader.height
+                        self.mainTableView.parallaxHeader.minimumHeight = tableHeader.height
+                        
+                        
+                        }, completion: { finished in
+                            print("View Moved2!")
+                    })
+            })
+        }
+    }
+    
+    
+    @IBAction func cancelButtonTap(sender: AnyObject) {
+        print("Button!")
+        if(!self.viewDown){
+            
+            
+            UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseOut, animations: {
+                
+                
+                let tableHeader = self.mainTableView.parallaxHeader
+                tableHeader.height = 400
+                self.mainTableView.parallaxHeader.height = tableHeader.height
+                self.mainTableView.parallaxHeader.minimumHeight = tableHeader.height
+                
+                
+                }, completion: { finished in
+                    print("View Moved!")
+                    self.tripPlannerBottomConstraint.constant = 19
+                    UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseOut, animations: {
+                        var tripPlannerFrame = self.tripPlannerView.frame
+                        tripPlannerFrame.origin.y = -156
+
+                        self.tripPlannerView.frame = tripPlannerFrame
+                        
+                        
+                        
+                        }, completion: { finished in
+                            print("View Moved2!")
+                    })
+            })
+        }
+
+    }
+    
+    @IBAction func searchButtonTap(sender: AnyObject) {
+        
+        UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseOut, animations: {
+            
+            
+            let tableHeader = self.mainTableView.parallaxHeader
+            tableHeader.height = 110
+            self.mainTableView.parallaxHeader.height = tableHeader.height
+            self.mainTableView.parallaxHeader.minimumHeight = tableHeader.height
+            
+            
+            }, completion: { finished in
+                print("View Moved!")
+            })
+        }
+
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
