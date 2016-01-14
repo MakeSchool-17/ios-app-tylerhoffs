8db158b9be51626b33e918611eac56fddb34e905
tylerhoffs <tyler.hoffman@students.makeschool.com>
2016-01-14 14:59:07 -0800

    Option page links updates

--- Transit Wise/ViewControllers/OptionsViewController.swift
+++ Transit Wise/ViewControllers/OptionsViewController.swift
@@ -9,15 +9,18 @@
 import UIKit
 import RealmSwift
 
-class OptionsViewController: UIViewController {
+class OptionsViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
     
+    @IBOutlet weak var versionLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var appIconImage: UIImageView!
     @IBOutlet weak var headerLabel: UILabel!
     @IBOutlet weak var optionsSegmentedControl: UISegmentedControl!
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
     let realmHelper =  RealmHelper()
     var agencies: [Agency]?
-    var links: [String] = ["Rate us on the App Store", "Send Feedback", "Follow @transitwise", "Like TransitWise on Facebook", "Share"]
+    var links: [String] = ["Send Feedback", "Follow @transitwise", "Like TransitWise on Facebook", "Share"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -71,8 +74,29 @@ class OptionsViewController: UIViewController {
             return cell
         }
     }
+    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: false)
+        if(indexPath.row == 0){
+            let email = "support@transitwise.co.za"
+            let url = NSURL(string: "mailto:\(email)")
+            UIApplication.sharedApplication().openURL(url!)
+        }
+        else if(indexPath.row == 1){
+            UIApplication.sharedApplication().openURL(NSURL(string: "https://twitter.com/transitwise")!)
+        }
+        else if(indexPath.row == 2){
+            UIApplication.sharedApplication().openURL(NSURL(string: "https://www.facebook.com/transitwise/")!)
+        }
+        else if(indexPath.row == 3){
+            let shareString = "String to share"
+            
+            let objectsToShare = [shareString]
+            
+            let activityViewController      = UIActivityViewController(activityItems: objectsToShare as [AnyObject], applicationActivities: nil)
+            
+            presentViewController(activityViewController, animated: true, completion: nil)
+        }
     }
     
     @IBAction func doneButtonTap(sender: AnyObject) {
@@ -81,10 +105,29 @@ class OptionsViewController: UIViewController {
     @IBAction func segmentChange(sender: AnyObject) {
         tableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: UITableViewRowAnimation.Fade)
         if(optionsSegmentedControl.selectedSegmentIndex == 0){
-            headerLabel.hidden = false
+            UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseOut, animations: {
+                self.tableView.transform = CGAffineTransformMakeTranslation(0, 0)
+                self.headerLabel.hidden = false
+                self.versionLabel.hidden = true
+                self.nameLabel.hidden = true
+                self.appIconImage.hidden = true
+                
+                }, completion: { finished in
+                    print("View Moved!")
+            })
         }
         else{
-            headerLabel.hidden = true
+            
+            UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseOut, animations: {
+                self.tableView.transform = CGAffineTransformMakeTranslation(0, 200)
+                self.headerLabel.hidden = true
+                self.versionLabel.hidden = false
+                self.nameLabel.hidden = false
+                self.appIconImage.hidden = false
+
+                }, completion: { finished in
+                    print("View Moved!")
+            })
         }
     }
     
